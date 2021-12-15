ab0c2c2b32c3b555fa11fd97e5ffa6676391f43d
ilanvys <ilanvys@gmail.com>
2021-12-15 09:44:18 +0200

    Add the snake and LinkedList impl

--- LinkedList.py
+++ LinkedList.py
@@ -1,19 +1,142 @@
-from typing import Any
+from typing import Any, List, Tuple
+from __future__ import annotations
+
+class Node:
+    """
+    Class Node is an implementation to a node in a linked list.
+    Node has data and a pointer to the next Node.
+    """
+    def __init__(self, data: Tuple[int, int] = None, next: Node=None) -> None:
+        """
+        des: Node constructor creates a new node with data if recieved,
+             and pointer to next Node if recieved.
+        params: Node constructor creates a new node with data recieved
+        """
+        self.__data = data
+        self.__next  = next
+
+    def __str__(self) -> str:
+        """
+        return: string representation of Node's data
+        """
+        return str(self.__data)
+
+    def get_data(self) -> Tuple[int, int]:
+        """
+        return: the Node's data
+        """
+        return self.__data
+
+    def get_next(self) -> Node:
+        """
+        return: pointer to the next Node
+        """
+        return self.__next
+
+    def set_data(self, data: Tuple[int, int]) -> None:        
+        """
+        des: set the Node's data to the data recieved
+        params: data: the data to set
+        """
+        self.__data = data
+
+    def set_next(self, next: Node) -> None:
+        """
+        des: set the next Node's data to the data recieved
+        params: data: the data to set the next node with
+        """
+        self.__next = next
 
 
 class LinkedList:
-    def __init__(self, next=None, value=None):
-        self.__next = next
-        self.__value = value
+    """
+    LinkedList class wrappes Node and provieds an API to use a linked 
+    list of nodes.
+    """
+    def __init__(self, head: Node = None) -> None:
+        """
+        des: Node constructor creates a new list with node as head if
+             recieved, otherwise head will be empty
+        params: List constructor creates a new list with  the
+                Node recieved as the head of the list
+        """
+        self.__head = head
 
-    def get_next(self):
-        return self.__next
+    def get_head(self) -> Any:
+        """
+        return: List's head Node, None if list is empty
+        """
+        return self.__head
 
-    def set_next(self, next):
-        self.__next = next
+    def is_empty(self) -> bool:
+        """
+        return: True if list is empty, False otherwise
+        """
+        return self.__head == None
 
-    def get_value(self):
-        return self.__value
+    def add(self, data: Tuple[int, int]) -> None:
+        """
+        des: adds a Node to the end of the list, with the node's data
+             recieved as a param.
+        params: data: the data to add to the list's tail
+        """
+        if self.is_empty():
+            self.__head = Node(data)
+            return
+        if self.get_head().get_next() == None:
+            self.get_head().set_next(Node(data))
+            return
+        
+        iterator = self.get_head()
+        
+        while(iterator.get_next()):
+            iterator = iterator.get_next()
+        iterator.set_next(Node(data))
+    
+    def add_head(self, new_head: Tuple[int, int]) -> None:
+        """
+        des: adds a Node to the head of the list, with the node's data
+             recieved as a param.
+        params: new_head: the data to add to the list's head
+        """
+        nh = Node(new_head)
+        nh.set_next(self.__head)
+        self.__head = nh
 
-    def set_value(self, value):
-        self.__value = value
+        
+    def remove_tail(self) -> None:
+        """
+        des: Removes the lists last cell
+        """
+        if self.is_empty():
+            return None
+        
+        if self.get_head().get_next() == None:
+            self.__head = None
+            return
+        
+        second_last = self.get_head()
+        
+        while(second_last.get_next().get_next()):
+            second_last = second_last.get_next()
+        second_last.set_next(None)
+        
+    def get_list(self) ->  List[Tuple[int, int]]:
+        """
+        return: a list of the snake's coordinates represnted
+                by tuples
+        """
+        lst_data = []
+        if self.is_empty():
+            return []
+
+        lst_data.append(self.get_head().get_data())
+        
+        iterator = self.get_head()
+        
+        while(iterator.get_next()):
+            iterator = iterator.get_next()
+            lst_data.append(iterator.get_data())
+        
+
+        return lst_data
